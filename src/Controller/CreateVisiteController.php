71063b604b92c78800bafc12aedaccbc57acf840
inzoudine.hamlaoui <hamlaoui.inzoudine@lycee-faure.fr>
2023-05-26 15:20:17 +0200

    update

--- src/Controller/CreateVisiteController.php
+++ src/Controller/CreateVisiteController.php
@@ -16,15 +16,25 @@ class CreateVisiteController extends AbstractController
     #[Route('/', name: 'app_create_visite')]
     public function index(Request $request, ManagerRegistry $doctrine): Response
     {
+        $jauge = 10;
+        $erreurA = "";
+        $erreurE = "";
         $lesExpos = $doctrine->getRepository(Exposition::class)-> findAll();
         $uneVisite = new Visite();
         $uneVisite->setDateHeureArrivee(new \DateTime('now'));
         $uneVisite->setDateHeureDepart(new \DateTime('now'));
         $uneVisite->setNbVisiteursAdultes(0);
         $uneVisite->setNbVisiteursEnfants(0);
-        $nbAdultes = $request ->get('nbAdultes');
-        $nbEnfants = $request ->get('nbEnfants');
-        $i = 1;
+        $ajout = null;
+
+
+        if($request ->get('nbAdultes') !== null){
+            $uneVisite->setNbVisiteursAdultes($request ->get('nbAdultes'));
+        }
+        if($request ->get('nbEnfants') !== null){
+            $uneVisite->setNbVisiteursEnfants($request ->get('nbEnfants'));
+        }
+
 
         foreach ($lesExpos as $expo)
         {
@@ -37,9 +47,27 @@ class CreateVisiteController extends AbstractController
             $request->get($i);
         }*/
 
+
+
+        if($request->get('nbAdultes') > $jauge){
+            $erreurA = "le nombre maximum d'adulte est dépassé";
+        }
+        if($request->get('nbEnfants') > $jauge){
+            $erreurE = "le nombre maximum d'enfant est dépassé";
+        }
+        if($request->get('valider')!==$ajout){
+            $doctrine->getManager()->persist($uneVisite);
+            $doctrine->getManager()->flush();
+        }
+
         return $this->render('create_visite/index.html.twig', [
             'lesExpos' => $lesExpos,
+            'visite' => $uneVisite,
+            'erreurA' => $erreurA,
+            'erreurE' => $erreurE,
         ]);
+
+
 
     }
 }
